06383ada888ba3119efebe37e9166dac6eca3597
SoulBuzz034 <soulbuzz034@gmail.com>
2024-06-28 14:36:54 +0300

    Add files via upload
    дз2 по основному курсу

--- homework2.py
+++ homework2.py
@@ -1,8 +1,6 @@
-name = "Nikita"
-print(name)
-age = 29
-print(age)
-age = age + 1
-print(age)
-is_student = age > 10
-print(is_student)
+number_of_tasks = 12
+number_of_hours = 1.5
+name = 'Phyton'
+time_for_one = number_of_hours / number_of_tasks
+print('На одно задание изучая курс', name,', ' 'мы тратим', time_for_one ,'часа')
+
